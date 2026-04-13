c3c3738865227975d4cfdd04c8a4bee79c6901fd
jimmy <jimishukurow@gmail.com>
2026-04-13 11:28:16 +0500

    Add authentication endpoint and user ID management to AuthServiceClient and UserContext

--- src/main/java/info/jemsit/common/UserContext.java
+++ src/main/java/info/jemsit/common/UserContext.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class UserContext {
     public static final ThreadLocal<List<Roles>> userRole = new ThreadLocal<>();
     public static final ThreadLocal<String> userToken = new ThreadLocal<>(); //be careful with this token, it can be User token or Agent token, it is  not always Agent Token
+    public static final ThreadLocal<Long> userId = new ThreadLocal<>(); //be careful with this token, it can be User id or Agent id, it is  not always Agent id
     public static void userRole(List<Roles> roles) {
         userRole.set(roles);
     }
@@ -20,6 +21,8 @@ public class UserContext {
     public static String getUserToken() {
         return userToken.get();
     }
+    public static void setUserId(Long id) {userId.set(id);}
+    public static Long getUserId() {return userId.get();}
 
     public static void clear() {
         userToken.remove();
